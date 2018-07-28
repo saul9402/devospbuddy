51c24fdf36db7fa5f15091f203c241b8b80a630f
saul9402 <saul_harry_@hotmail.com>
2018-07-27 22:55:37 -0500

    Agregando capa de idioma (i18n)

---
+++ src/main/java/com/devopsbuddy/config/I18NConfig.java
@@ -0,0 +1,19 @@
+package com.devopsbuddy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class I18NConfig {
+
+	@Bean
+	public ReloadableResourceBundleMessageSource messageSource() {
+		ReloadableResourceBundleMessageSource resourceBundleMessageSource = new ReloadableResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("classpath:i18n/messages");
+		// Revisa si hay nuevos mensajes cada 30 minutos
+		resourceBundleMessageSource.setCacheSeconds(1800);
+		return resourceBundleMessageSource;
+	}
+
+}
